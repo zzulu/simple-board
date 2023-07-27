f40ec07f74df5e3f4533a150d1ce4544b4ddbd11
Junwoo Hwang <zzulu.hwang@gmail.com>
2023-07-27 09:43:50 +0900

    Add: status to Post, Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,16 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+
+      redirect_to post_path(@post), status: :see_other
+  end
+
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_001732) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_003901) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -25,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_001732) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   add_foreign_key "comments", "posts"
